074fb94d2a927ec46555c41d6f7c6593791eea07
EdwardVNguyen <edwardnguyen568@gmail.com>
2026-01-04 23:02:17 -0600

    12-14 finished

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let left = 0 
+  let right = string.length - 1
 
+  while (left < right) {
+    // if left is not alphabet, move on
+    while (!(string[left].match(/[a-z0-9]/i)))
+      left++
+    // same with right side
+    while (!(string[right].match(/[a-z0-9]/i)))
+      right--
+    // check if both letters are equivalent to see if it is a palidrome
+    if (string[left].toLowerCase() !== string[right].toLowerCase()) 
+      return false
+    // progress through the string
+    left++ 
+    right--
+  }
+  return true
 };
 
 // Do not edit below this line
