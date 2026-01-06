71515a109ebf7148cb4200a2504f39070918cd17
EdwardVNguyen <edwardnguyen568@gmail.com>
2026-01-05 18:49:13 -0600

    1-11 finished

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+  for (const arg of args) {
+    arr = arr.filter(num => num !== arg) // I had done != previously, which didn't work because it didn't type check. e.g. "1" and 1 were considered the same
+  } 
+  return arr
 };
 
 // Do not edit below this line
--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+  // yay propositional logic, goes back to discrete math
+  if ( (year % 4 === 0) && (!(year % 100 === 0) || (year % 400 === 0)) )
+    return true
+  else 
+    return false
 };
 
 // Do not edit below this line
